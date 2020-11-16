d11de1c68ff9040df7ccd68ac181b83c39d71611
Quinton Fults <quinton.fults.work@gmail.com>
2020-11-16 13:06:44 -0800

    code

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const fs = require('fs')
 //   if (err) { console.log(err) }
 // })
 
-fs.writeFile('arr.js', '[1,2,3,4,5]', (err) => {
-  if (err) { console.log(err) }
+// fs.writeFile('arr.js', '[1,2,3,4,5]', (err) => {
+//   if (err) { console.log(err) }
 
-  fs.writeFile('arr.js', '["a","b","c"]', (err) => {
-    if (err) { console.log(err) }
-  })
-})
+//   fs.writeFile('arr.js', '["a","b","c"]', (err) => {
+//     if (err) { console.log(err) }
+//   })
+// })
 
+// fs.appendFile('example.txt', ' Goodbye World!', (err) => {
+//   if (err) { console.log(err) }
+// })
+
+// fs.readFile('example.txt', 'utf8', (err, text) => {
+//   if (err) { console.log(err) }
+//   console.log(text)
+// })
