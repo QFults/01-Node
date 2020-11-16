4bad2a8af76367b29510d9f658c6c1f6d3ef163e
Quinton Fults <quinton.fults.work@gmail.com>
2020-11-16 11:42:24 -0800

    code

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-const a = parseInt(process.argv[2])
-const b = parseInt(process.argv[3])
+// const a = parseInt(process.argv[2])
+// const b = parseInt(process.argv[3])
 
-console.log(a + b)
+// console.log(a + b)
+
+
+const op = process.argv[2]
+const a = parseInt(process.argv[3])
+const b = parseInt(process.argv[4])
+
+switch (op) {
+  case 'add':
+    console.log(a + b)
+    break
+  case 'subtract':
+    console.log(a - b)
+    break
+  case 'multiply':
+    console.log(a * b)
+    break
+  case 'divide':
+    console.log(a / b)
+    break
+}
