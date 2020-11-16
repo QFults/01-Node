1a138d57399f3ec0bf38b3668c92a1d42dc399e7
Quinton Fults <quinton.fults.work@gmail.com>
2020-11-16 13:32:44 -0800

    code

---
+++ dog.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+let dog = {
+  name: 'Beef',
+  age: 2,
+  breed: 'chihuahua'
+}
+
+// fs.writeFile('dog.html', '', (err) => {
+//   if (err) { console.log(err) }
+
+  // fs.appendFile('dog.html', `
+  // <h1>${dog.name}</h1>
+  // <h2>${dog.age}</h2>
+  // <h3>${dog.breed}</h3>
+  // `, (err) => {
+  //   if (err) { console.log(err) }
+  // })
+// })
+
+// fs.readFile('dog.html', 'utf8', (err, html) => {
+//   if (err) { console.log(err) }
+//   console.log(html)
+// })
