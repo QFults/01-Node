79c8d168b7a30cd28252cc8593e0975ee5836e1b
Quinton Fults <quinton.fults.work@gmail.com>
2020-11-17 10:37:05 -0800

    code

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // let person = require('./person.js')
 
 // console.log(person)
-
+const fs = require('fs')
 const movies = require('./movies.js')
 const songs = require('./songs.js')
 const foods = require('./foods.js')
 const games = require('./games.js')
 
-console.log(movies)
-console.log(songs)
-console.log(foods)
-console.log(games)
+fs.writeFile('favorites.txt', `
+  ${movies.join(', ')}, ${songs.join(', ')}, ${foods.join(', ')}, ${games.join(', ')}
+`, (err) => {
+  if (err) { console.log(err) }
+})
